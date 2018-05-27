bdaa7688306a3287208a3d451ac2cc9a333dbc8c
Miraion <j.schwartz564@icloud.com>
2018-05-26 23:55:35 -0700

    T -> Any
    Removed the generic type T in the Console.log methods and replaced it with the Any identifier.

--- server/Sources/WebHost/Log.swift
+++ server/Sources/WebHost/Log.swift
@@ -9,14 +9,14 @@
 public class Console {
     
     /// Prints a message to stdout and calls `fflush` after.
-    public static func log<T>(_ msg: T) {
+    public static func log(_ msg: Any) {
         print(msg)
         fflush(stdout)
     }
     
     
     /// Prints a message prepending `"Error: "` to stdout calls `fflush` after.
-    public static func log<T>(error msg: T) {
+    public static func log(error msg: Any) {
         print("Error: \(msg)")
         fflush(stdout)
     }
