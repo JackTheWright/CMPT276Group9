c43546562513c1d673c35b4e71fcae0c2dc317a8
Miraion <j.schwartz564@icloud.com>
2018-06-03 16:24:44 -0700

    Xcode Shared Data Update

--- TrackITServer/Tests/NetConnectTests/NetConnectTests.swift
+++ TrackITServer/Tests/NetConnectTests/NetConnectTests.swift
@@ -2,15 +2,27 @@ import XCTest
 @testable import NetConnect
 
 final class NetConnectTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssert(true)
+    
+    func testMessage() {
+        do {
+            let msg = Message()
+            msg.body = Data(raw: "Hello World")
+            let encodedMsg = msg.encoded()
+            let decodedMsg = try Message(messageData: encodedMsg)
+            let originalStr = "Hello World"
+            if let decodedStr = String(bytes: decodedMsg.body) {
+                print(originalStr)
+                print(decodedStr)
+                XCTAssert(decodedStr == originalStr)
+            } else {
+                XCTAssert(false)
+            }
+        } catch {
+            XCTAssert(false)
+        }
     }
 
-
     static var allTests = [
-        ("testExample", testExample),
+        ("testMessage", testMessage),
     ]
 }
