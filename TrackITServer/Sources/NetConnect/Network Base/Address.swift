3abe7ca132fda951a23e4120596f6406c40a15fd
Jeremy <j.schwartz564@icloud.com>
2018-06-26 22:11:39 -0700

    NetConnect Implementation 1

---
+++ TrackITServer/Sources/NetConnect/Network Base/Address.swift
@@ -0,0 +1,40 @@
+import Foundation
+import Socket
+
+/// Wrapper class for Socket.Address.
+public class Address {
+    
+    var addr: Socket.Address
+    
+    let hostname: String?
+    
+    /// Constructs an address for a given host on a specific port.
+    ///
+    /// - parameters:
+    ///     - hostname: IPv4 address or fully qualified domain name for the
+    ///         desired host.
+    ///
+    ///     - port: The port to connect on. Must be within the range 0 to 65535.
+    ///
+    /// Returns `nil` if unable to create the underlying `Socket.Address`
+    /// object.
+    init?(hostname: String, port: Int) {
+        self.hostname = hostname
+        if port < 0 || port > UInt16.max { return nil }
+        if let a = Socket.createAddress(for: hostname, on: Int32(port)) {
+            self.addr = a
+        } else {
+            return nil
+        }
+    }
+    
+    /// Constructs an address from an existing `Socket.Address` object.
+    ///
+    /// - parameters:
+    ///     - addr: An existing `Socket.Address` object to wrap around.
+    init(_ addr: Socket.Address) {
+        self.addr = addr
+        self.hostname = nil
+    }
+    
+}
