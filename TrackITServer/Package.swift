857e07fe4161cd7b3826705ff5db7787b88a6f63
Jeremy <j.schwartz564@icloud.com>
2018-07-03 18:43:01 -0700

    Dependency Update
    SwiftyJSON is now a local dependency instead of a remote dependency.

--- TrackITServer/Package.swift
+++ TrackITServer/Package.swift
@@ -6,15 +6,16 @@ let package = Package(
     name: "Server",
     products: [
         .library(name: "NetConnect", targets: ["NetConnect"]),
+        .library(name: "SwiftyJSON", targets: ["SwiftyJSON"]),
         .executable(name: "Server", targets: ["Server"])
     ],
     dependencies: [
         .package(url: "https://github.com/Miraion/Threading.git", from: "1.0.0"),
         .package(url: "https://github.com/IBM-Swift/BlueSocket.git", from: "1.0.0"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift", from: "0.0.0"),
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.0.0")
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift", from: "0.0.0")
     ],
     targets: [
+        .target(name: "SwiftyJSON", dependencies: []),
         .target(name: "NetConnect", dependencies: ["Socket", "CryptoSwift", "SwiftyJSON"]),
         .target(name: "Server", dependencies: ["NetConnect", "Threading"]),
         .testTarget(name: "NetConnectTests", dependencies: ["NetConnect"])
--- TrackITServer/Sources/Server/main.swift
+++ TrackITServer/Sources/Server/main.swift
@@ -17,31 +17,23 @@ import SwiftyJSON
 let port = 60000
 
 func client() {
-    do {
-        let netif = try NetworkInterface()
-        netif.connect(to: "192.168.10.24", on: port) { host in
-            let greeting = "Hello Server, I'm \(IFAddress.localIP() ?? "nil")"
-            print("Sending : \(greeting)")
-            try host.send(greeting)
-            let response = try host.receiveString()
-            print("Received: \(response)")
-        }
-    } catch let e {
-        print(e.localizedDescription)
+    let netif = NetworkInterface()!
+    netif.connect(to: "192.168.10.24", on: port) { host in
+        let greeting = "Hello Server, I'm \(IFAddress.localIP() ?? "nil")"
+        print("Sending : \(greeting)")
+        try host.send(greeting)
+        let response = try host.receiveString()
+        print("Received: \(response)")
     }
 }
 
 func server() {
-    do {
-        let netif = try NetworkInterface()
-        netif.listen(on: port) { host in
-            let greeting = try host.receiveString()
-            print("Received: \(greeting)")
-            let response = "Hello Client, I'm \(IFAddress.localIP() ?? "nil")"
-            print("Sending : \(response)")
-            try host.send(response)
-        }
-    } catch let e {
-        print(e.localizedDescription)
+    let netif = NetworkInterface()!
+    netif.listen(on: port) { host in
+        let greeting = try host.receiveString()
+        print("Received: \(greeting)")
+        let response = "Hello Client, I'm \(IFAddress.localIP() ?? "nil")"
+        print("Sending : \(response)")
+        try host.send(response)
     }
 }
