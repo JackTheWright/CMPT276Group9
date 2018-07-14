07b83ca6a4538ec3e3d71f0755f8acb9e7231614
Jeremy <j.schwartz564@icloud.com>
2018-07-14 12:39:09 -0700

    PerfectSQLite Dependency

--- TrackITServer/Package.swift
+++ TrackITServer/Package.swift
@@ -12,12 +12,13 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Miraion/Threading.git", from: "1.0.1"),
         .package(url: "https://github.com/IBM-Swift/BlueSocket.git", from: "1.0.0"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift", from: "0.0.0")
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift", from: "0.0.0"),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-SQLite.git", from: "3.0.0")
     ],
     targets: [
         .target(name: "SwiftyJSON", dependencies: []),
         .target(name: "NetConnect", dependencies: ["Socket", "CryptoSwift", "SwiftyJSON"]),
-        .target(name: "Server", dependencies: ["NetConnect", "Threading"]),
+        .target(name: "Server", dependencies: ["NetConnect", "Threading", "PerfectSQLite"]),
         .testTarget(name: "NetConnectTests", dependencies: ["NetConnect"])
     ]
 )
--- TrackITServer/Sources/Server/main.swift
+++ TrackITServer/Sources/Server/main.swift
@@ -12,6 +12,7 @@ import NetConnect
 import CryptoSwift
 import SwiftyJSON
 import Threading
+import PerfectSQLite
 
 
 let isServer = false
