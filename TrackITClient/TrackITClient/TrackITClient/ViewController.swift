90ff5ea84e13f6379e12b41f2b860e95d6c7fe83
Jeremy <j.schwartz564@icloud.com>
2018-07-15 13:01:07 -0700

    Sudo Merge User-Info -> master

--- TrackITClient/TrackITClient/TrackITClient/ViewController.swift
+++ TrackITClient/TrackITClient/TrackITClient/ViewController.swift
@@ -13,17 +13,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate{
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    //     textField.resignFirstResponder()
+    //   return true
+    //}
+    
 }
 
