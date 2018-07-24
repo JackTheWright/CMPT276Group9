f7150a3c2c92bea13c8a808ec5e4b95d9b339c46
Chloe0416 <39498946+Chloe0416@users.noreply.github.com>
2018-07-24 10:24:39 +0800

    Update and rename extendCrypto.swift to AESCryptographer.swift

--- TrackITServer/Sources/NetConnect/Delegates/AESCryptographer.swift
+++ TrackITServer/Sources/NetConnect/Delegates/AESCryptographer.swift
@@ -1,22 +1,25 @@
 //
-//  extendCrypto.swift
+//  AESCryptographer.swift
 // Module       : NetConnect
 //
 // Team Name    : Group 9
 // Created By   : Keyi Huang
 // Created On   : 2018-07-21
 
-import UIKit
 import Function
 import CryptoSwift
 
 class extendCrypto: Cryptographer{
+    private let key: String
+    private let iv: String
     
-    let key = "1234567890987654321"
-    let iv = "1234567890987654321"
+    public init ((key: String, iv: String){
+        self.key = key
+        self.iv = iv
+    }
     let aes = try AES(key: key.bytes, blockMode: .CBC(iv: iv.bytes))
     
-    func encrypt(_ data: Data) throws -> Data {
+    public func encrypt(_ data: Data) throws -> Data {
         
         let bytes = data.bytes
         let encrypted = try aes.encrypt(bytes)
@@ -25,7 +28,7 @@ class extendCrypto: Cryptographer{
         
     }
 
-    func decrypt(_ data: Data) throws -> Data{
+    public func decrypt(_ data: Data) throws -> Data{
         
         let bytes = data.bytes
         let decrypted = try aes.decrypt(bytes)
