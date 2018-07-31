4b92d7651ac936fb9560c496232ed853074a40ff
Sidd <sga94@sfu.ca>
2018-07-30 19:32:07 -0700

    Implemented Daily view (Pie chart done) needs UI fixes

--- TrackITClient/TrackITClient/TrackITClient/DailyViewController.swift
+++ TrackITClient/TrackITClient/TrackITClient/DailyViewController.swift
@@ -7,14 +7,97 @@
 //
 
 import UIKit
+import Charts
 
 class DailyViewController: UIViewController {
 
+    @IBOutlet weak var pieChart: PieChartView!
+    
     @IBOutlet weak var todayLbl: UILabel!
+    
+    var grp1Data = PieChartDataEntry(value: 0)
+    var grp2DaTa = PieChartDataEntry(value: 0)
+    var grp3Data = PieChartDataEntry(value: 0)
+    var grp4Data = PieChartDataEntry(value: 0)
+    var grp5Data = PieChartDataEntry(value: 0)
+    
+    var dailyMealsEntry = [PieChartDataEntry]()
+    var todaysValues = [String]()
+    var valuesDic = [String : [String]]()
+    var currentDate = String()
+    var dailyVals = UserDefaults.standard.dictionary(forKey: "dailyEntries") as? [String : [String : [String]]] ?? ["" : ["" : ["0","0","0","0","0"]]]
+    
+    func currDateFormatter() -> String {
+        let date = Date()
+        let dateformatter = DateFormatter()
+        dateformatter.dateFormat = "dd-mm-yyyy"
+        return dateformatter.string(from: date)
+    }
+    
+    
+    
+        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+         // Do any additional setup after loading the view.
+        
+        // set chart description
+        pieChart.chartDescription?.text = "Servings of different food groups consumed today :"
+        
+        // checking if dailyVals has default value (no foods) or has foods
+        if dailyVals !=  ["" : ["" : ["0","0","0","0","0"]]] {
+            // extract the dictionaries
+            for each in dailyVals {
+                valuesDic.update(other: each.value)
+            }
+            
+            // get current date in appropiate string format
+            currentDate = currDateFormatter()
+            
+            // check which entry is for current date
+            for each in valuesDic {
+                if each.key == currentDate {
+                    // assign the values for servings of each foodgroup
+                    todaysValues = each.value
+                    break
+                }
+            }
+        }
+        
+        else {
+            todaysValues = ["1","2","2","3","4"]
+        }
+        
+        
+        grp1Data.value = Double(Int(todaysValues[0])!)
+        grp1Data.label = "Meats"
+        
+        grp2DaTa.value = Double(Int(todaysValues[1])!)
+        grp2DaTa.label = "Vegetables"
+        
+        grp3Data.value = Double(Int(todaysValues[2])!)
+        grp3Data.label = "Fruits"
+        
+        grp4Data.value = Double(Int(todaysValues[3])!)
+        grp4Data.label = "Dairy"
+        
+        grp5Data.value = Double(Int(todaysValues[4])!)
+        grp5Data.label = "Grains"
+       
+        dailyMealsEntry = [grp1Data, grp2DaTa, grp3Data, grp4Data, grp5Data]
+        updateChart()
+    }
+    
+    func updateChart() {
+        let chartDataSet = PieChartDataSet(values: dailyMealsEntry, label: nil)
+        let chartData = PieChartData(dataSet: chartDataSet)
+        
+        let color = [UIColor.red, UIColor.green, UIColor.yellow, UIColor.yellow, UIColor.blue]
+        chartDataSet.colors = color
+        
+        pieChart.data = chartData
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
